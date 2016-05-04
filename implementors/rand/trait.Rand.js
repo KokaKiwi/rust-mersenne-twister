dced17377efe7ca546f42c25e3fb8a40b9ece4ea
KokaKiwi <kokakiwi@kokakiwi.net>
2016-05-05 04:06:45 +0900

    Update documentation

---
+++ implementors/rand/trait.Rand.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rand'] = [];implementors['rand_mersenne_twister'] = ["impl <a class='trait' href='rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='struct' href='rand_mersenne_twister/mt32/struct.MTRng32.html' title='rand_mersenne_twister::mt32::MTRng32'>MTRng32</a>","impl <a class='trait' href='rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='struct' href='rand_mersenne_twister/mt64/struct.MTRng64.html' title='rand_mersenne_twister::mt64::MTRng64'>MTRng64</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
